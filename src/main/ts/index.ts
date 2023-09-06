30acd763365567ac05007344fe119a9c0dc0fd02
Snap <XxSnapperGeexX@gmail.com>
2023-09-06 00:46:35 -0700

    Add header and main body fluid container.

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,5 +1,8 @@
 import $ from "jquery";
 
-const $h1 = $("<h1>").text("The Weather");
+const $h1 = $("<h1>").addClass(["text-white", "fw-bold"]).text("The Weather");
+const $header = $("<header>").addClass(["d-flex", "align-items-center", "justify-content-center"]).append($h1);
 
-$("body").append($h1);
+const $container = $("<main>").addClass(["container-fluid"]);
+
+$("body").append($header, $container);
