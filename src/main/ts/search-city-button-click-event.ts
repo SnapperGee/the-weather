dd7c4901206efd128d32f4af2b05acd8db4fb6bb
Snap <XxSnapperGeexX@gmail.com>
2023-09-07 18:23:02 -0700

    Add validate function to search-city-button-click-event module.

--- src/main/ts/search-city-button-click-event.ts
+++ src/main/ts/search-city-button-click-event.ts
@@ -1,15 +1,36 @@
-const stringIsValid = (aString: string): string | false =>
-{
-    const formattedString = aString.trim().replace(/\s{2,}/, "\u0020");
+const isNotBlank: RegExp = /\S+/;
+const commaSurroundedByNonBlank =  /\S+.*,.*\S+/;
 
-    if (formattedString.length !== 0)
-    {
-        return formattedString;
-    }
-    else
+const isValidCitySearchQuery = (aString: string): boolean =>
+{
+    // empty/blank string is invalid
+    if ( ! isNotBlank.test(aString))
     {
         return false;
     }
+
+    let commaCharCount = 0;
+
+    // string that contains more than 1 comma or a comma that doesn't have non-whitespace on both sides
+    if (aString.includes(","))
+    {
+        // make sure string only has 1 (or less) commas
+        for (const char of aString)
+        {
+            if (char === "," && ++commaCharCount > 1)
+            {
+                return false;
+            }
+        }
+
+        // make sure string has at least 1 non-whitespace character on each side of comma
+        if ( ! commaSurroundedByNonBlank.test(aString))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 export const searchCityButtonClickEvent = ( mouseEvent: MouseEvent,
@@ -18,14 +39,14 @@ export const searchCityButtonClickEvent = ( mouseEvent: MouseEvent,
 {
     mouseEvent.preventDefault();
 
-    let formattedHtmlInput: string | false;
+    let formattedHtmlInput: string;
 
-    if (htmlInputElement.checkValidity() && (formattedHtmlInput = stringIsValid(htmlInputElement.value)))
-    {
-        console.log("Valid city search input.");
-    }
-    else
-    {
-        console.log("Invalid city search input.");
-    }
+    // if (htmlInputElement.checkValidity() && (formattedHtmlInput = stringNotEmptyOrBlank(htmlInputElement.value)))
+    // {
+    //     console.log("Valid city search input.");
+    // }
+    // else
+    // {
+    //     console.log("Invalid city search input.");
+    // }
 }
