4fd019f0a4d5af97be0bba3f7334e3af4607185b
Snap <XxSnapperGeexX@gmail.com>
2023-09-07 18:25:47 -0700

    City search query function into module in new city-search-query directory.

--- src/main/ts/search-city-button-click-event.ts
+++ src/main/ts/search-city-button-click-event.ts
@@ -1,38 +1,3 @@
-const isNotBlank: RegExp = /\S+/;
-const commaSurroundedByNonBlank =  /\S+.*,.*\S+/;
-
-const isValidCitySearchQuery = (aString: string): boolean =>
-{
-    // empty/blank string is invalid
-    if ( ! isNotBlank.test(aString))
-    {
-        return false;
-    }
-
-    let commaCharCount = 0;
-
-    // string that contains more than 1 comma or a comma that doesn't have non-whitespace on both sides
-    if (aString.includes(","))
-    {
-        // make sure string only has 1 (or less) commas
-        for (const char of aString)
-        {
-            if (char === "," && ++commaCharCount > 1)
-            {
-                return false;
-            }
-        }
-
-        // make sure string has at least 1 non-whitespace character on each side of comma
-        if ( ! commaSurroundedByNonBlank.test(aString))
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 export const searchCityButtonClickEvent = ( mouseEvent: MouseEvent,
                                             htmlInputElement: NonNullable<HTMLInputElement>,
                                             htmlLIElement: NonNullable<HTMLUListElement> ): void =>
