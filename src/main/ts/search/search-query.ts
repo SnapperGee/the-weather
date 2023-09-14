659d2f9183e04875f89496052f1eaf6733b72c82
Snap <XxSnapperGeexX@gmail.com>
2023-09-14 12:35:33 -0700

    Add doc comments.

--- src/main/ts/search/search-query.ts
+++ src/main/ts/search/search-query.ts
@@ -1,3 +1,7 @@
+/**
+ * @module search-query
+ */
+
 const isNotBlankRegex: RegExp = /\S+/;
 const commaSurroundedByNonBlank: RegExp = /\S+.*,.*\S+/;
 const twoOrMoreWhiteSpaces = /\s{2,}/g;
@@ -6,6 +10,16 @@ const commaFollowedBy2AdjacentNonWhitespaceChars = /^,\s*\S{2}\s*$/;
 export const isNotBlank = (aString: string): boolean => isNotBlankRegex.test(aString);
 export const isBlank = (aString: string): boolean => ! isNotBlankRegex.test(aString);
 
+/**
+ * Checks to see if a provided `string` is a valid city search query format. That is, either a single string containing
+ * no commas and at least one non-whitespace character, such as "San Francisco", or a string containing a single comma
+ * with at least one non-whitespace character before the comma and only 2 non-whitespace characters after the comma such
+ * as "Berkeley, US".
+ *
+ * @param aString `string` to check if it's a valid city search query format.
+ *
+ * @returns `true` if the passed `string` argument is formatted correctly for a city search query.
+ */
 export const isValidSearchFormat = (aString: string): boolean =>
 {
     // empty/blank string is invalid
@@ -44,7 +58,24 @@ export const isValidSearchFormat = (aString: string): boolean =>
     return true;
 }
 
+/**
+ * Formats a single `string` to a consistent format.
+ *
+ * @param aString `string` to format to a consistent city search query format.
+ *
+ * @returns the formatted `string`.
+ */
 export function formatSearchQuery(aString: string): string;
+
+/**
+ * Formats an array of strings to a consistent format. It removes all leading and trailing whitespace from each string
+ * and capitalizes the first character of the first string and converts the second string to uppercase. So
+ * " san   francisco  , uS " would become "San Francisco, US".
+ *
+ * @param strings An array of `string`s to format to a consistent format.
+ *
+ * @returns An array containing the formatted `string`s.
+ */
 export function formatSearchQuery(strings: string[]): string[];
 export function formatSearchQuery(aStringOrStrings: string | string[]): string | string[]
 {
